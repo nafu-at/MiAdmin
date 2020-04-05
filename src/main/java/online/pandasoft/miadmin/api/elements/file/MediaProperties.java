193f51ec585029dfe6367fad0f0641e75fb83e4e
nafu_at <koucha@fuu-music.moe>
2020-04-06 01:22:29 +0900

    :construction: Fixed an erroneous copyright notice.

--- src/main/java/online/pandasoft/miadmin/api/elements/file/MediaProperties.java
+++ src/main/java/online/pandasoft/miadmin/api/elements/file/MediaProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 なふちょこ.
+ * Copyright 2020 PandaSoft.Dev Social Networks.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
